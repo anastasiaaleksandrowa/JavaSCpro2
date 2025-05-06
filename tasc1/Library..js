d193d59d95d0e25c441416ff5875bcc15bad5f13
Kira <kirnan_t@outlok.com>
2025-05-06 13:39:11 +0700

    принимаем начальный список книг

---
+++ tasc1/Library..js
@@ -0,0 +1,13 @@
+class Library {
+
+    #
+    books;
+
+    constructor(initialBooks = []) {
+        const uniqueBooks = [...new Set(initialBooks)];
+        if (uniqueBooks.length !== initialBooks.length) {
+            throw new Error("Начальный список книг не должен содержать дубликатов.");
+        }
+        this.#books = uniqueBooks;
+    }
+}
